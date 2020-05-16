0ee14a4c04294f4ac921c08b03e431d48e4049a5
gPlacide <gmizplacide@gmail.com>
2020-05-15 19:56:01 -0700

    made changes

--- examples.py
+++ examples.py
@@ -5,6 +5,7 @@ import copy
 # HINT: I recommend you plot these boards
 board0 = Board([Territory(territory_id=i, player_id=1, armies=i%5+1) for i in range(42)])
 
+
 board1 = copy.deepcopy(board0)
 board1.set_owner(3,0)
 board1.set_armies(3,500)
@@ -13,6 +14,10 @@ board1.set_armies(34,500)
 board1.set_armies(39,500)
 board1.set_armies(35,500)
 
+#x= board1.is_valid_attack_path([3,28])
+#print('x=', x)
+
+
 board2 = copy.deepcopy(board1)
 board2.set_owner(34, 0)
 board2.set_armies(34,1)
@@ -20,7 +25,7 @@ board2.set_owner(35, 0)
 board2.set_armies(35,1)
 board2.set_owner(39, 0)
 board2.set_armies(39,1)
-board2.plot_board()
+#board2.plot_board()
 
 board3 = copy.deepcopy(board2)
 board3.set_owner(34, 1)
@@ -35,8 +40,11 @@ board5 = Board([Territory(territory_id=i, player_id=i%5, armies=i%4+1) for i in 
 path=board5.cheapest_attack_path(3,18)
 path=board5.cheapest_attack_path(3,6)
 print("path=",path)
-asd
-#board5.plot_board(path=[1,19])
+#asd
+path = [1, 13]
+board1.plot_board(path=path)
+#board0 = Board([Territory(territory_id=i, player_id=1, armies=i%5+1) for i in range(42)])board0 = Board([Territory(territory_id=i, player_id=1, armies=i%5+1) for i in range(42)])board1.plot_board(path=path)
+
 #board5.plot_board(plot_graph=True,filename='board5_graph.png')
 #board5.plot_board(plot_graph=False,filename='board5.png')
 #board5.plot_board(plot_graph=True)
